9f4b6b7d795070a145e292fd607df2c5d6a3e5cc
Anthony <anthony.ducret@lilo.org>
2024-05-11 16:12:44 +0200

    Lab 5, draft

--- lab5/main.py
+++ lab5/main.py
@@ -8,34 +8,31 @@ sigma_p0 = 10  # m
 sigma_v0 = 0.1  # m/s
 
 # Uncertainties of motion model
-sigma_v = 0.05  # m/s² / Hz
+sigma_a = 0.05  # m/s² / Hz
 
 
 def main():
     # Apply random seed for repeatability
     np.random.seed(42)
 
-    # Reference position
+    # Reference state (x, y, vx, vy)
+    time = ref.generate_time_serie()
     ref_states = ref.generate_ref_states()
 
+    # GPS noise model
+    gps_noise = WhiteNoise(freq=ref.FREQ, duration=ref.SIMULATION_TIME, sd=0.5)
+
     # Generate realizations
-    time = ref.generate_time_serie()
     for real in range(ref.NUMBER_REALIZATION):
 
-        # Gps
-        gps_noise = WhiteNoise(freq=ref.FREQ, duration=ref.SIMULATION_TIME, sd=0.5)
-        gps_states = np.copy(ref_states)
-        gps_states[:, 0] = gps_noise.add_noise(ref_states[:, 0])  # x
-        gps_states[:, 1] = gps_noise.add_noise(ref_states[:, 1])  # y
-        # gps_states[:, 2:]                                       # v_ref    x and y
-        # Set all states out of gps frequency to None
+        # Gps measurement, noise on position @ ref freq
+        gps_time = ref.generate_time_serie(freq=ref.GPS_FREQ)
+        gps_states = ref.generate_ref_states(freq=ref.GPS_FREQ)
+        gps_states[:, 0] = gps_noise.add_noise(gps_states[:, 0])  # x
+        gps_states[:, 1] = gps_noise.add_noise(gps_states[:, 1])  # y
 
-
-        # Kalman filter
-        x0 = np.array([
-            ref.X_0, ref.Y_0, ref.V_X_0, ref.V_Y_0
-        ])
-
+        # Kalman filter parameters
+        x0 = ref_states[0]
         p0 = np.array([
             [sigma_p0, 0, 0, 0],
             [0, sigma_p0, 0, 0],
@@ -43,9 +40,24 @@ def main():
             [0, 0, 0, sigma_v0],
         ])
 
-        phi = None
-        q = None
-        h = None
+        dt = ref.DT
+
+
+        F =
+        phi = np.array([
+            [1, 0, dt, 0],
+            [0, 1, 0, dt],
+            [0, 0, 0, 0],  # Predicted velocity x ?
+            [0, 0, 0, 0],  # Predicted velocity y ?
+
+        ])
+        q = np.array([
+
+        ])
+        h = np.array([
+            [1, 0, 0, 0],
+            [0, 1, 0, 0],
+        ])
         kf = KalmanFilter(x0, p0, phi, q, h)
 
         for i, t in enumerate(time):
@@ -55,10 +67,6 @@ def main():
                 z = None
                 r = None
                 kf.add_measure(z, r)
-
-
-
-
 
 
 if __name__ == '__main__':
--- lab5/src/reference.py
+++ lab5/src/reference.py
@@ -6,53 +6,37 @@ SIMULATION_TIME = 200
 RADIUS = 25  # m
 OMEGA = np.pi / 100  # rad/s
 FREQ = 100
+DT = 1/FREQ
 GPS_FREQ = 1
 
 # Initial conditions
-ALPHA_0 = 0  # rad, Orientation with vertical axis in clockwise direction
-X_0 = 0  # m
-Y_0 = RADIUS  # m
-V_X_0 = OMEGA*RADIUS
-V_Y_0 = 0
+THETA_0 = 0  # rad, Orientation with vertical axis in clockwise direction
 
 
-# reference state
-
-def x_ref(t):
-    return RADIUS * np.sin(ALPHA_0 + OMEGA * t)  # X : horizontal axis
-
-
-def y_ref(t):
-    return RADIUS * np.cos(ALPHA_0 + OMEGA * t)  # Y : vertical axis
-
-
-def vx_ref(t):
-    v = OMEGA * RADIUS
-    return v * np.cos(ALPHA_0 + OMEGA * t)
-
-
-def vy_ref(t):
-    v = OMEGA * RADIUS
-    return v * np.sin(ALPHA_0 + OMEGA * t)
-
-
-def generate_time_serie(freq=None):
-    if freq is None:
-        freq = FREQ
-    time = np.arange(start=0, step=1 / FREQ, stop=SIMULATION_TIME)
+def generate_time_serie(freq=FREQ):
+    time = np.arange(start=0, step=1 / freq, stop=SIMULATION_TIME)
     return time
 
 
-def generate_ref_states(freq=None):
+def generate_ref_states(freq=FREQ):
     time = generate_time_serie(freq)
-    x = x_ref(time)
-    y = y_ref(time)
-    v_x = vx_ref(time)
-    v_y = vy_ref(time)
-    states = np.array([
-        x,
-        y,
-        v_x,
-        v_y
-    ]).T
+    theta = THETA_0 + OMEGA * time
+    x = RADIUS * np.sin(theta)
+    y = RADIUS * np.cos(theta)
+
+    v = OMEGA * RADIUS
+    vx = v * np.cos(theta)
+    vy = v * np.sin(theta)
+
+    # a = OMEGA ** 2 * RADIUS
+    # ax = - a * np.sin(theta)
+    # ay = - a * np.cos(theta)
+    # states = np.array([x, y, vx, vy, ax, ay]).T
+
+    states = np.array([x, y, vx, vy]).T
     return states
+
+
+if __name__ == "__main__":
+    ref_states = generate_ref_states()
+    print(ref_states[0:2])
